616c13843339559f279d6a497fa16560a20169cb
goodness5 <goodnesskolapo@gmail.com>
2022-10-05 11:24:50 +0100

    update

--- eazycoin/settings.py
+++ eazycoin/settings.py
@@ -125,6 +125,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATICFILES_DIR = os.path.join(BASE_DIR, '/static')
+MEDIA_URL = '/ media /'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
--- eazycoin/urls.py
+++ eazycoin/urls.py
@@ -17,10 +17,15 @@ from xml.etree.ElementInclude import include
 from django.contrib import admin
 from django.urls import include, path
 from accounts import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path(r'', views.index, name='index'),
     path('accounts/', include('accounts.urls')),
     path('wallet/',  include('wallet.urls')),
-]
+    
+    ]
+if  settings.DEBUG :
+    urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
