bda1584d6a61d3da5a664632c09f06471d18eb18
goodness5 <goodnesskolapo@gmail.com>
2022-10-20 15:42:50 +0100

    static

--- eazycoin/settings.py
+++ eazycoin/settings.py
@@ -44,7 +44,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'accounts.apps.AccountsConfig',
     'wallet.apps.WalletConfig',
-    'newsletter.apps.newsletterConfig'
+    'newsletter',
 ]
 
 MIDDLEWARE = [
