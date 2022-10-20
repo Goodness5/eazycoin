13132b726e26541171b6f805e4b79c81a5e29b89
goodness5 <goodnesskolapo@gmail.com>
2022-10-20 13:16:34 +0100

    j

--- accounts/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-# from .models import signup
-from .models import profile
-
-# Register your models here.
-#admin.site.register(signup)
-admin.site.register(profile)
--- newsletter/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import CustomUser, SubscribedUsers
-
-class SubscribedUsersAdmin(admin.ModelAdmin):
-    list_display = ('email', 'name', 'created_date')
-
-admin.site.register(CustomUser)
-admin.site.register(SubscribedUsers, SubscribedUsersAdmin)
