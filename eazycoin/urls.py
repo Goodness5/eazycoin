ef0880f6fad6ecd8c03e5055baa14cbf45c6d323
goodness5 <goodnesskolapo@gmail.com>
2022-11-02 13:41:51 +0100

    hosts allowed update

--- eazycoin/urls.py
+++ eazycoin/urls.py
@@ -13,10 +13,8 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-# from unicodedata import name
+
 from accounts import views
-# from eazycoin import accounts
-# from wallet import views
 from django.contrib import admin
 from django.urls import path
 from django.urls import include
